aa199be89150711da73ed190e3798269b0adb98f
Alexseusik <alexgnibeda03@gmail.com>
2024-09-25 12:33:03 +0000

    change

--- chat_text_gen.py
+++ chat_text_gen.py
@@ -5,9 +5,14 @@ from datetime import datetime
 
 today_date = datetime.now().strftime('%d.%m.%Y')
 
+<<<<<<< HEAD
 apikey = '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'
 
 openai.api_key = apikey
+=======
+apikey = "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"
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+>>>>>>> 3c726cd (updating)
 client = OpenAI(api_key=apikey)
 
 
